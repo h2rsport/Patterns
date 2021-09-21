ca3c2a4c1e96b4a83208c278a57a91b7327e0d4e
h2rsport <91111903+h2rsport@users.noreply.github.com>
2021-09-21 16:17:44 +0530

    Update and rename README.md to star.py

--- star.py
+++ star.py
@@ -2,10 +2,12 @@ for i in range(6):
     for j in range(i+1):
         print("* ",end="")
     print()
+    
 for i in range(6):
         for j in range(6):
             print("* ", end="")
         print()
+        
 for i in range(6):
     for j in range(6-i):
         print("* ",end="")
